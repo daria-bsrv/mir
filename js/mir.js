1357dc05ff675fd63fdcc13b81e151194ef3265a
Дарья <basyrova84@inbox.ru>
2025-04-29 12:59:50 +0500

    Add files via upload

---
+++ js/mir.js
@@ -0,0 +1,17 @@
+//задание 1
+const p = document.querySelector(".test-text");
+console.log(p);
+p.textContent= "Привет, мир! Меня зовут Даша";
+
+//задание 2
+const text = document.getElementById(".email");
+console.log(text);
+text.textContent="mail@mail.ru";
+
+//задание 3
+const firstlink = document.querySelector(".midis");
+console,log(firstlink);
+firstlink.setAttribute("href", "https://midis.ru/"); //установить атрибут или изменить значение существующего
+firstlink.setAttribute("target", "_blank");
+
+//задание 4
